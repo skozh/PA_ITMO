a264f0736d27448ba5d0b2b592c12a38574d1420
SHYJU KOZHISSERI <shyjukozhisseri@outlook.com>
2020-12-01 18:27:20 +0530

    Initial

---
+++ Task2.cpp
@@ -0,0 +1,98 @@
+/* Matrix Multiplication */
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+using namespace std;
+
+
+double multiplyMatrices(int A[][100], int B[][100], int C[][100], int size){
+    
+    auto t1 = std::chrono::system_clock::now();
+    for (int i=0; i<size; i++){
+        for (int j=0; j<size; j++){
+            C[i][j]=0;
+            for (int k=0; k<size; k++){
+                C[i][j]+= A[i][k]*B[k][j];
+            }
+        }
+    }
+    auto t2 = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count(); 
+}
+
+
+double multiplyMatricesParallel(int A[][100], int B[][100], int D[][100], int size){
+    int i, j, k;
+    omp_set_dynamic(0);
+    omp_set_num_threads(size);
+    #pragma omp parallel for private(i,j,k)
+    {
+        auto t1 = std::chrono::system_clock::now();
+        for (i=0; i<size; i++){
+            for (j=0; j<size; j++){
+                D[i][j]=0;
+                for (k=0; k<size; k++){
+                    D[i][j]+= A[i][k]*B[k][j];
+                }
+            }
+        }
+        auto t2 = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count(); 
+    }
+
+}
+
+
+void displayMatrix(int A[100][100], int size){
+    for (int i=0; i< size; i++){
+        for (int j=0; j<size; j++){
+            cout<<A[i][j];
+            cout<<"\t";
+        }
+        cout<<"\n";
+    }
+}
+
+
+int main()
+{
+
+    int A[100][100], B[100][100], C[100][100], D[100][100], size;
+    std::clock_t start, end, t1, t2;
+    double duration_1, duration_2;
+    srand(time(0));
+    
+    cout<<"Enter size of the square matrices: ";
+    cin>>size;
+
+    for (int i=0; i< size; i++){
+        for (int j=0; j<size; j++){
+            A[i][j]=1+rand()%9;
+            B[i][j]=1+rand()%9;
+        }
+    }
+
+    cout<<"Matrix A\n";
+    displayMatrix(A, size);
+
+    cout<<"Matrix B\n";
+    displayMatrix(B, size);
+
+
+    //Sequential method
+    duration_1 = multiplyMatrices(A, B, C, size);
+
+    //Parallel method
+    duration_2 = multiplyMatricesParallel(A, B, D, size);
+    
+    cout<<"Matrix C\n";
+    displayMatrix(C, size);
+
+    cout<<"Matrix D\n";
+    displayMatrix(D, size);
+
+    cout<<"Sequential Time (ns): "<<to_string(duration_1)<<"\n";
+    cout<<"Parallel Time (ns): "<<to_string(duration_2)<<"\n";
+    
+    return 0;
+}
